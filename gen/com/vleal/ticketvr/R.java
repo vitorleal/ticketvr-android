0939cdc8c50c60c73a4a3ebf3f111fd92c6c472c
Vitor Leal <vitorleal1@gmail.com>
2013-11-21 15:03:51 -0200

    added custom style

--- gen/com/vleal/ticketvr/R.java
+++ gen/com/vleal/ticketvr/R.java
@@ -10,6 +10,10 @@ package com.vleal.ticketvr;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int default_ticketvr_color=0x7f040001;
+        public static final int pressed_ticketvr=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,31 +21,67 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ab_background_textured_ticketvr=0x7f020000;
+        public static final int ab_bottom_solid_ticketvr=0x7f020001;
+        public static final int ab_solid_ticketvr=0x7f020002;
+        public static final int ab_stacked_solid_ticketvr=0x7f020003;
+        public static final int ab_texture_tile_ticketvr=0x7f020004;
+        public static final int ab_transparent_ticketvr=0x7f020005;
+        public static final int btn_cab_done_default_ticketvr=0x7f020006;
+        public static final int btn_cab_done_focused_ticketvr=0x7f020007;
+        public static final int btn_cab_done_pressed_ticketvr=0x7f020008;
+        public static final int btn_cab_done_ticketvr=0x7f020009;
+        public static final int cab_background_bottom_ticketvr=0x7f02000a;
+        public static final int cab_background_top_ticketvr=0x7f02000b;
+        public static final int ic_launcher=0x7f02000c;
+        public static final int list_focused_ticketvr=0x7f02000d;
+        public static final int menu_dropdown_panel_ticketvr=0x7f02000e;
+        public static final int pressed_background_ticketvr=0x7f02000f;
+        public static final int progress_bg_ticketvr=0x7f020010;
+        public static final int progress_horizontal_ticketvr=0x7f020011;
+        public static final int progress_primary_ticketvr=0x7f020012;
+        public static final int progress_secondary_ticketvr=0x7f020013;
+        public static final int selectable_background_ticketvr=0x7f020014;
+        public static final int spinner_ab_default_ticketvr=0x7f020015;
+        public static final int spinner_ab_disabled_ticketvr=0x7f020016;
+        public static final int spinner_ab_focused_ticketvr=0x7f020017;
+        public static final int spinner_ab_pressed_ticketvr=0x7f020018;
+        public static final int spinner_background_ab_ticketvr=0x7f020019;
+        public static final int tab_indicator_ab_ticketvr=0x7f02001a;
+        public static final int tab_selected_focused_ticketvr=0x7f02001b;
+        public static final int tab_selected_pressed_ticketvr=0x7f02001c;
+        public static final int tab_selected_ticketvr=0x7f02001d;
+        public static final int tab_unselected_focused_ticketvr=0x7f02001e;
+        public static final int tab_unselected_pressed_ticketvr=0x7f02001f;
+        public static final int tab_unselected_ticketvr=0x7f020020;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int pager=0x7f080000;
-        public static final int section_label=0x7f080001;
+        public static final int action_settings=0x7f090002;
+        public static final int pager=0x7f090000;
+        public static final int section_label=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int fragment_main_dummy=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int my_cards=0x7f050002;
-        public static final int no_save=0x7f050003;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int my_cards=0x7f060002;
+        public static final int no_save=0x7f060003;
     }
     public static final class style {
+        public static final int ActionBar_Solid_Ticketvr=0x7f070003;
+        public static final int ActionBar_Transparent_Ticketvr=0x7f070004;
+        public static final int ActionBarTabStyle_Ticketvr=0x7f070007;
+        public static final int ActionButton_CloseMode_Ticketvr=0x7f07000a;
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
@@ -63,10 +103,18 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+        public static final int DropDownListView_Ticketvr=0x7f070006;
+        public static final int DropDownNav_Ticketvr=0x7f070008;
+        public static final int PopupMenu_Ticketvr=0x7f070005;
+        public static final int ProgressBar_Ticketvr=0x7f070009;
+        public static final int Theme_Ticketvr=0x7f070002;
+        /**  this style is only referenced in a Light.DarkActionBar based theme 
+         */
+        public static final int Theme_Ticketvr_Widget=0x7f07000b;
     }
 }

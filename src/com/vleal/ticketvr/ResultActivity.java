1b9c9ea2866176579a075216a2c68f58155d193c
Vitor Leal <vitorleal1@gmail.com>
2013-11-22 18:22:12 -0200

    added values to the result activity

--- src/com/vleal/ticketvr/ResultActivity.java
+++ src/com/vleal/ticketvr/ResultActivity.java
@@ -1,8 +1,12 @@
 package com.vleal.ticketvr;
 
+import java.lang.reflect.Array;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.widget.TextView;
 
 public class ResultActivity extends Activity {
@@ -10,19 +14,26 @@ public class ResultActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_result);
 		
 		Intent intent = getIntent();
 		String value  = intent.getStringExtra("value");
 		String number = intent.getStringExtra("number");
-		//String date   = intent.getStringExtra("date");
 		
 		TextView cardNumber = (TextView) findViewById(R.id.cardNumber);
 		TextView money      = (TextView) findViewById(R.id.money);
 		
 		cardNumber.setText(number);
 		money.setText(value);
+	}
+	
+	public String FormatCard(String card) {
+		String[] cardArray = TextUtils.split(card, "\\d{1,4}");
+		Log.i("array", "" + cardArray);
+		String cardFormat  = TextUtils.join(" ", cardArray);
+		Log.i("card", "" + cardArray.toString());
 		
-		setContentView(R.layout.activity_result);
+		return cardFormat;
 	}
 
 	/*@Override
